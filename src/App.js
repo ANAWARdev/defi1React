de291dedf1d8b5d983bfff4c328251c3e9f53bca
ANAWARdev <143820904+ANAWARdev@users.noreply.github.com>
2024-06-20 21:51:11 +0200

    Création des composants

--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// Déclaration des components ou composants
+const Title = ({ content }) => <h1>{content}</h1>;
+const ComponentReact = () => {
+  return (
+    <>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      <br />
+    </>
+  );
+};
+
+const ComponentVue = () => {
+  return (
+    <>
+      <a
+        className="App-link"
+        href="https://vuejs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn Vue
+      </a>
+      <br />
+    </>
+  );
+};
+
+const ComponentAngular = () => {
+  return (
+    <>
+      <a
+        className="App-link"
+        href="https://angularjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn Angular
+      </a>
+      <br />
+    </>
+  );
+};
 
 function App() {
+  const libraries = ["Learn React", "Learn Angular", "Learn Vue"];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Title content="Welcome !" />
+        <ComponentReact />
+        <ComponentVue />
+        <ComponentAngular />
+        
       </header>
     </div>
   );
